dc62ed3ef3ae0cff76c428e2dd94c2a392ac7113
B--B <dark.schneider.ds@gmail.com>
2016-10-16 23:42:20 +0200

    OTAUpdates: update for N

--- device.mk
+++ device.mk
@@ -239,9 +239,9 @@ PRODUCT_PACKAGES += \
 
 # OTA Updates
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.ota.romname=AOSP-GEMINI-6.0 \
+    ro.ota.romname=AOSP-GEMINI-7.0 \
     ro.ota.version=$(shell date -u +%Y%m%d) \
-    ro.ota.manifest=http://romhut.com/roms/aosp-gemini-6-0/ota.xml
+    ro.ota.manifest=http://romhut.com/roms/aosp-gemini-7-0/ota.xml
 
 # Performance
 PRODUCT_COPY_FILES += \
