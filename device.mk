4941375e1f2baa94c2327664d2201c8508fd5b45
Rashed Abdel-Tawab <rashed@linux.com>
2017-03-12 19:01:31 -0400

    msm8996: Move power config into init.qcom.rc and add EAS'd perfd
    Half this power script is a grand mess anyway, so just import
    the parts used into init.qcom.rc and cleanup all the other power crap.
    Also add missing sepolicy for EAS enabled perfd
    
    Change-Id: I86563998aef207d5dc30fde116b37dd4a4717b55

--- device.mk
+++ device.mk
@@ -135,7 +135,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fstab.qcom \
     init.qcom.rc \
-    init.qcom.power.rc \
     init.qcom.usb.rc \
     init.qcom.usb.diag.rc \
     init.target.rc \
--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -40,14 +40,6 @@ LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := init.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.power.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := init.qcom.power.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 LOCAL_REQUIRED_MODULES := init.power.sh
 include $(BUILD_PREBUILT)
 
