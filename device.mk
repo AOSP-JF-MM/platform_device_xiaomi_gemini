2a7ce488408e5c6fefba1c004c7d838cef4ca781
B--B <dark.schneider.ds@gmail.com>
2017-03-17 16:14:01 +0100

    Revert "Add charger images to build"
    This reverts commit 61866708957ddb57c50c15fd3d66cabab4d8c6d5.
    
    Moved to our vendor repo

--- device.mk
+++ device.mk
@@ -303,10 +303,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libshims_ims
 
-# Charger images
-PRODUCT_PACKAGES += \
-    charger_res_images
-
 # WiFi
 PRODUCT_PACKAGES += \
     libqsap_sdk \
