1699d02d05f3dabc02f1b668346647b362f2a815
Myles Watson <mylesgw@google.com>
2017-03-31 11:25:04 -0700

    msm8996-common: Add libbt-vendor in device-common.mk
    Bug: 36810308
    Test: build
    Change-Id: Iae4a6340a184dde818603b546470c8cf883f2f7b
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- device.mk
+++ device.mk
@@ -113,6 +113,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:/system/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:/system/etc/usb_audio_policy_configuration.xml
 
+# Bluetooth
+PRODUCT_PACKAGES += \
+    libbt-vendor
+
 # Camera
 PRODUCT_PACKAGES += \
     SnapdragonCamera
