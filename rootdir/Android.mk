09d62049c7e70b28f396f7d04a746ba66d575b24
Luca Stefani <luca020400@lineageos.org>
2017-05-16 19:14:42 +0200

    msm8996-common: Improve SELinux policies
     * Move .sh scripts to /system/bin
     * Set a domain per script
     * Label panel color property
     * Switch to global macros
    
    Change-Id: Id989128551c5a2acfbe8ae7df08ed2e2703c3040

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -8,6 +8,7 @@ LOCAL_MODULE       := init.cnss.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.cnss.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
@@ -15,6 +16,7 @@ LOCAL_MODULE       := init.panel.sh
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.panel.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_EXECUTABLES)
 include $(BUILD_PREBUILT)
 
 # Device init scripts
