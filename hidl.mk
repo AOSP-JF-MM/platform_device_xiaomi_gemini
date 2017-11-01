c604360fc059d6a687b9f8d3580b9009b84a22ab
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-11-01 03:31:57 +0100

    hidl: add audio service and update manifest
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -16,6 +16,7 @@
 
 # Audio HIDL
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     android.hardware.broadcastradio@1.0-impl \
