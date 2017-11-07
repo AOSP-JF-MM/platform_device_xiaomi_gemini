dbf225c3501242d81aaeb5a70f659846663c34a5
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-11-07 01:16:04 +0100

    hidl: add soundtrigger service and rework audio impl/services list
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -16,11 +16,12 @@
 
 # Audio HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0-service \
     android.hardware.audio@2.0-impl \
+    android.hardware.audio@2.0-service \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.broadcastradio@1.0-impl \
-    android.hardware.soundtrigger@2.0-impl
+    android.hardware.audio.effect@2.0-service \
+    android.hardware.soundtrigger@2.0-impl \
+    android.hardware.soundtrigger@2.0-service
 
 # Bluetooth HIDL
 PRODUCT_PACKAGES += \
