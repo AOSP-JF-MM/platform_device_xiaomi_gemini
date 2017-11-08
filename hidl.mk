e05960194d4c9d62bbca26ba162ac9190aa9f9f4
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-11-08 18:58:05 +0100

    hidl: add bluetooth service
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -25,7 +25,8 @@ PRODUCT_PACKAGES += \
 
 # Bluetooth HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.bluetooth@1.0-impl
+    android.hardware.bluetooth@1.0-impl \
+    android.hardware.bluetooth@1.0-service
 
 # Camera
 PRODUCT_PACKAGES += \
