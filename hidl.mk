b4b479c402429dfdb339fa1c119506e9b737782d
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-11-08 18:53:50 +0100

    Hidl: add lights service
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- hidl.mk
+++ hidl.mk
@@ -73,7 +73,8 @@ PRODUCT_PACKAGES += \
 
 # Lights HIDL
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-impl
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Media HIDL
 PRODUCT_PACKAGES += \
