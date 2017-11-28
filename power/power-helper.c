790a1c24bb07b140bdef19a9f8fba436cbd7b277
Marco Zanin (B--B) <mrczn.bb@gmail.com>
2017-11-28 15:10:54 +0100

    Revert "msm8996: Set top-app boost to 10 based on hints rather than always"
    This reverts commit ebcc23843d1d37cb400f237006b9836a90acd285.
    
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- power/power-helper.c
+++ power/power-helper.c
@@ -438,9 +438,9 @@ void power_hint(power_hint_t hint, void *data)
 
             // Scheduler is EAS.
             if (strncmp(governor, SCHED_GOVERNOR, strlen(SCHED_GOVERNOR)) == 0) {
-                // Setting the value of foreground schedtune boost to 10 and
+                // Setting the value of foreground schedtune boost to 50 and
                 // scaling_min_freq to 900MHz.
-                int resources[] = {0x40800000, 900, 0x40800100, 900, 0x42C0C000, 0x10, 0x41800000, 0x33};
+                int resources[] = {0x40800000, 900, 0x40800100, 900, 0x42C0C000, 0x32, 0x41800000, 0x33};
                 interaction(duration, sizeof(resources)/sizeof(resources[0]), resources);
             } else { // Scheduler is HMP.
                 int resources[] = {0x41800000, 0x33, 0x40800000, 1000, 0x40800100, 1000, 0x40C00000, 0x1};
