1d2970c8364ab5079d959f6a664316b03e9798f3
Philip Koslowski <philip.koslowski@gmail.com>
2018-09-26 18:46:17 +0200

    build OmniClock

--- device.mk
+++ device.mk
@@ -310,3 +310,7 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini
+
+# OmniClock
+PRODUCT_PACKAGES += \
+	OmniClockOSS
